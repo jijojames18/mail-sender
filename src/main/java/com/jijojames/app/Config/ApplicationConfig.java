6bb9d0c9d2cb0102d46fd6ea57fce840a6e7fda6
Jijo James <jijojames18@gmail.com>
2020-08-26 16:15:25 +0530

    Get credential from environment

--- src/main/java/com/jijojames/app/Config/ApplicationConfig.java
+++ src/main/java/com/jijojames/app/Config/ApplicationConfig.java
@@ -1,20 +1,38 @@
 package com.jijojames.app.Config;
 
+import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 import org.springframework.context.event.EventListener;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Base64;
 
 @Component
 @EnableAutoConfiguration
 @ConfigurationProperties
 public class ApplicationConfig {
     private static final Logger logger = LoggerFactory.getLogger(ApplicationConfig.class);
+
+    @Autowired
+    private Environment env;
 
     @Value("${spring.application.firebase.auth-credentials-storage}")
     private String firebaseAuthStorageLocation;
@@ -45,6 +63,9 @@ public class ApplicationConfig {
 
     @Value("${spring.application.email.subject}")
     private String emailSubject;
+
+    @Value("${spring.application.gmail.stored-credential-env}")
+    private String gmailStoredCredentialEnv;
 
     public String getFirebaseAuthStorageLocation() {
         return firebaseAuthStorageLocation;
@@ -80,6 +101,10 @@ public class ApplicationConfig {
         return emailFromAddressEnv;
     }
 
+    public String getGmailStoredCredentialEnv() {
+        return gmailStoredCredentialEnv;
+    }
+
     public String getEmailSubject() {
         return emailSubject;
     }
@@ -95,6 +120,7 @@ public class ApplicationConfig {
                 "spring.application.gmail.token-path-env : " + getGmailTokenPathEnv() + "\n" +
                 "spring.application.gmail.application-name: " + getGmailApplicationName() + "\n" +
                 "spring.application.email.from-address-env : " + getEmailFromAddressEnv() + "\n" +
+                "spring.application.gmail.stored-credential-env : " + getGmailStoredCredentialEnv() + "\n" +
                 "spring.application.email.subject : " + getEmailSubject() + "\n" +
                 "spring.application.recaptcha.site-key : " + getRecaptchaSiteKeyEnv() + "\n"
         );
@@ -102,5 +128,22 @@ public class ApplicationConfig {
 
     public Boolean isConfigInEnv() {
         return firebaseAuthStorageLocation.equals("env");
+    }
+
+    @Bean
+    public JsonFactory jsonFactory(){
+        return JacksonFactory.getDefaultInstance();
+    }
+
+    @Bean
+    public void environmentVariableClientSecrets() throws Exception {
+        restoreEnvironmentVariableToFile(getGmailStoredCredentialEnv(), env.getProperty(getGmailTokenPathEnv()));
+    }
+
+    private void restoreEnvironmentVariableToFile(String environmentVariableName, String pathName) throws IOException {
+        Files.createDirectories(Paths.get(pathName));
+        Files.write(Paths.get(pathName +
+                        File.separator + "StoredCredential"),
+                Base64.getDecoder().decode(env.getProperty(environmentVariableName)));
     }
 }
